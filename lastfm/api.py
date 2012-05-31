9dbb5eb09c48aed0f9bf9b80308a855a0f277c65
filipovskii_off <filipovskii.off@gmail.com>
2012-05-31 15:52:36 +0400

    More comments!! MORrrREE!!

--- lastfm/api.py
+++ lastfm/api.py
@@ -11,6 +11,16 @@ ROOT_URL = 'http://ws.audioscrobbler.com/2.0/'
 SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 def create_session():
+    """Get username and session key.
+
+    Request a token from lastfm api.
+    Open browser, so user could authorize your app.
+    Create lastfm session.
+    Save user name and session key to `~/.lastfmsession`.
+
+    If session key and user name are already in `~/.lastfmsession`,
+    return them without sending any requests.
+    """
     try:
         with open(environ['HOME'] + '/.lastfmsession', 'r') as session_file:
 
@@ -50,16 +60,24 @@ def create_session():
         return name, key
 
 def get_top_artists_for_user(username):
+    """Call *user.getTopArtists* method.
+
+    Description: http://www.last.fm/api/show/user.getTopArtists
+    """
     return _call_lastfm_api('user.getTopArtists', user=username)
 
 def get_artist_info(artist_name):
-    print('Getting ' + artist_name)
+    """Call *artist.getInfo* method.
+    
+    Description: http://www.last.fm/api/show/artist.getInfo
+    """
     return _call_lastfm_api(
             'artist.getInfo',
-             # workaround, non-ascii chars not supported..
+            # non-ascii chars not supported..
             artist=artist_name.encode('utf-8'))
 
 def _call_lastfm_api(method_name, **method_args):
+    """Call lastfm method with given parameters, return result JSON"""
     method_args['api_key'] = API_KEY
     method_args['method'] = method_name
     url = _make_request_url(**method_args)
@@ -68,14 +86,20 @@ def _call_lastfm_api(method_name, **method_args):
 
 
 def _make_request_url(**kwargs):
+    """Create URL for regular GET request"""
     url = ROOT_URL[:] + '?'
     kwargs['format'] = 'json'
     url += urllib.urlencode(kwargs)
-    print(url)
     return url
 
     
 def _make_signed_request_url(**kwargs):
+    """Create URL for signed GET request
+    
+    More on this: http://www.last.fm/api/authspec
+    (see `8. Signing Calls`)
+
+    """
     md5 = hashlib.md5()
     for key in sorted(kwargs):
         md5.update('{}{}'.format(key, kwargs[key]))
@@ -85,7 +109,7 @@ def _make_signed_request_url(**kwargs):
     
 
 if __name__ == '__main__':
-    username, session_key = create_session()
-    top_artists_dict = get_top_artists_for_user(username)
-    print(top_artists_dict['topartists']['artist'][0])
-    print(get_artist_info('The Mars Volta'))
+    # username, session_key = create_session()
+    # top_artists_dict = get_top_artists_for_user(username)
+    # print(top_artists_dict['topartists']['artist'][0])
+    print(get_artist_info(u'Emilíana Torrini'))
